240011eb9818a89e0929f85cfbcca5cf8d7e3725
daimiya <mi.dai.tomo@gmail.com>
2026-08-31 05:24:10 +0900

    色々修正

--- src/ros2can/src/ros2can_node.cpp
+++ src/ros2can/src/ros2can_node.cpp
@@ -18,9 +18,14 @@ ROS2CAN_Node::ROS2CAN_Node() : Node("udp_bridge_node"), sockfd_(-1), is_running_
         "udp_can_tx", 10, std::bind(&ROS2CAN_Node::tx_callback, this, std::placeholders::_1)
     );
 
-    bldc_rx_publisher_ = this->create_publisher<ros2can::msg::BLDCDriver>("BLDC_RX", 10);
-    bldc_tx_subscription_ = this->create_subscription<ros2can::msg::BLDCDriver>(
+    bldc_rx_publisher_ = this->create_publisher<ros2can::msg::BLDCRX>("BLDC_RX", 10);
+    bldc_tx_subscription_ = this->create_subscription<ros2can::msg::BLDCTX>(
             "BLDC_TX", 10, std::bind(&ROS2CAN_Node::BLDC_callback, this, std::placeholders::_1)
+    );
+    
+    pwr_rx_publisher_ = this->create_publisher<ros2can::msg::PWRManagerRX>("PWRManager_RX", 10);
+    pwr_tx_subscription_ = this->create_subscription<ros2can::msg::PWRManagerTX>(
+            "PWRManager_TX", 10, std::bind(&ROS2CAN_Node::PWR_callback, this, std::placeholders::_1)
     );
 
     // Setup UDP Socket
@@ -53,6 +58,10 @@ ROS2CAN_Node::ROS2CAN_Node() : Node("udp_bridge_node"), sockfd_(-1), is_running_
 
     // Start receiver thread
     rx_thread_ = std::thread(&ROS2CAN_Node::rx_thread_func, this);
+    std::memset(&remote_addr, 0, sizeof(remote_addr));
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(remote_port_);
+    remote_addr.sin_addr.s_addr = inet_addr(remote_ip_.c_str());
 }
 
 ROS2CAN_Node::~ROS2CAN_Node() {
@@ -73,12 +82,6 @@ void ROS2CAN_Node::tx_callback(const ros2can::msg::UdpCanFrame::SharedPtr msg) {
     packet.id = msg->id;
     packet.size = msg->size;
     memcpy(packet.data, msg->data.data(), std::min(static_cast<size_t>(msg->size), sizeof(packet.data)));
-    struct sockaddr_in remote_addr;
-    std::memset(&remote_addr, 0, sizeof(remote_addr));
-    remote_addr.sin_family = AF_INET;
-    remote_addr.sin_port = htons(remote_port_);
-    remote_addr.sin_addr.s_addr = inet_addr(remote_ip_.c_str());
-
     ssize_t sent_bytes = sendto(sockfd_, &packet, sizeof(packet), 0, (const struct sockaddr *)&remote_addr, sizeof(remote_addr));
         
     if (sent_bytes < 0) {
@@ -86,10 +89,55 @@ void ROS2CAN_Node::tx_callback(const ros2can::msg::UdpCanFrame::SharedPtr msg) {
     }
 }
 
-void ROS2CAN_Node::BLDC_callback(const ros2can::msg::BLDCDriver::SharedPtr msg) {
-  RCLCPP_WARN(this->get_logger(), "Received BLDCDriver message: board_num=%d, rps_target=%d, angle_target=%d",
-        msg->board_num, msg->rps_target, msg->angle_target);
+void ROS2CAN_Node::BLDC_callback(const ros2can::msg::BLDCTX::SharedPtr msg)  {
+    UdpPacket packet;
+    std::memset(&packet, 0, sizeof(packet));
+
+    ID id;
+    id.fields.priority = msg->priority;
+    id.fields.data_type = DataType::BLCD_COMANND;
+    id.fields.board_num = msg->board_num;
+
+    BLDCPacket bldc_packet;
+    bldc_packet.mode = msg->mode;
+    bldc_packet.rps_target = msg->rps_target;
+    bldc_packet.angle_target = msg->angle_target;
+
+    packet.id = id.id;
+    packet.size = 32;
+
+    
+    memcpy(packet.data, &bldc_packet, 9);
+    ssize_t sent_bytes = sendto(sockfd_, &packet, sizeof(packet), 0, (const struct sockaddr *)&remote_addr, sizeof(remote_addr));
+
+    if (sent_bytes < 0) {
+        RCLCPP_WARN(this->get_logger(), "Failed to send UDP packet");
+    }
 }
+void ROS2CAN_Node::PWR_callback(const ros2can::msg::PWRManagerTX::SharedPtr msg)  {
+    UdpPacket packet;
+    std::memset(&packet, 0, sizeof(packet));
+
+    ID id;
+    id.fields.priority = msg->priority;
+    id.fields.data_type = DataType::POWERBOARD_COMANND;
+    id.fields.board_num = msg->board_num;
+
+    PWRPacket pwr_packet;
+    pwr_packet.pwrstatus = msg->powerstatus;
+    pwr_packet.ledstatus = msg->ledstatus;
+
+    packet.id = id.id;
+    packet.size = 32;
+    
+    memcpy(packet.data, &pwr_packet, sizeof(pwr_packet));
+    ssize_t sent_bytes = sendto(sockfd_, &packet, sizeof(packet), 0, (const struct sockaddr *)&remote_addr, sizeof(remote_addr));
+
+    if (sent_bytes < 0) {
+        RCLCPP_WARN(this->get_logger(), "Failed to send UDP packet");
+    }
+}
+
 
 void ROS2CAN_Node::rx_thread_func() {
     UdpPacket packet;
